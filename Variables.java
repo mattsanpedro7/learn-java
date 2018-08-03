186696295f8d1df4dc83ed443496900975a49295
mattsanpedro76 <mattsanpedro76@gmail.com>
2018-08-03 08:35:19 -0700

    add variables

---
+++ Variables.java
@@ -0,0 +1,32 @@
+// Java is a strong typed language (ie variables need to be defined before we use them)
+
+public class Variables {
+    public static void main(String[] args) {
+        // Numbers: to declare
+        int myNumber;
+        myNumber = 5 * 98;
+        
+        System.out.println("My number is: " + myNumber);
+        // int myNumber = 5;
+        
+        
+        // Double floating point number
+        double d = 4.5;
+        d = 3.0 * 20;
+        
+        System.out.println("My double is: " + d);
+
+        // float
+        float f = (float) 4.5;
+        
+        // f is a shorter way to cast float
+        // float f = 4.5f;
+
+        System.out.println("My float is: " + f);
+        
+        // characters and strings
+        char c = 'g';
+        System.out.println("My character is: " + c);
+
+    }
+}
