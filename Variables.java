4f3dff783df196f015f2d5a6ae8203ee92835265
mattsanpedro7 <mattsanpedro7@gmail.com>
2018-08-03 16:39:54 -0700

    finish variables exercise

--- Variables.java
+++ Variables.java
@@ -28,5 +28,48 @@ public class Variables {
         char c = 'g';
         System.out.println("My character is: " + c);
 
+        // string with a constructor
+        String s1 = new String("Who let the perros out?");
+
+        // Just using "" create a string
+        String s2 = "Who let the cat out?";
+
+        // Use + to concatenate
+        String s3 = s1 + "  " + s2;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        // concat string to primitives:
+        int num = 5;
+        String ss = "I have " + num + " cookies";
+
+        System.out.println(ss);
+
+        // boolean
+        boolean b = false;
+        b = true;
+        boolean toBe = false;
+        b = toBe || !toBe;
+        
+        if (b) {
+            System.out.println(toBe);
+        }
+        
+        System.out.println(b);
+
+        // exercise
+        char q = 'H';
+        int x = 311;
+        char r = 'w';
+        int y = 0;
+        int z = 1;
+        float g = (float) 2.0;
+        boolean myBool = true;
+        byte zero = 0;
+        String output = "H" + x + zero + " W" + zero + "r" + z + "d " + g + " " + myBool;
+
+        System.out.println(output);
     }
 }
